8f9c0b08ada1eb9e90c222551b4f71cc5c68c293
tochi <tochi@TOCHI-PC-BIG>
2024-01-26 02:13:41 +0000

    Add project files.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿string secret = "lgbtq";
+int numOfLives = 3;
+
+Console.WriteLine("You have 3 lives.");
+Console.Write("Enter the secret word: ");
+string choice = Console.ReadLine();
+numOfLives--;
+
+while (choice != secret && numOfLives > 0)
+{
+    Console.WriteLine("You have {0} lives.", numOfLives);
+    Console.Write("Enter the secret word: ");
+    choice = Console.ReadLine();
+    numOfLives--;
+}
+
+if  (choice == secret)
+{
+    Console.WriteLine(" You win");
+}
+else
+{
+    Console.WriteLine(" You Lose");
+
+}
