43cf222fe653fc87c70b160e1a59cfba7e0d4e0b
tochi <tochimba26@gmail.com>
2024-01-26 04:50:52 +0000

    objects and onject methods

---
+++ HelloCsharp/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCsharp
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages) { 
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+        public bool IsNovel()
+        {
+            if (pages > 200)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string FirstName()
+        {
+            return author.Split(" ")[0];
+        }
+    }
+}
--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,25 +1,51 @@
-﻿string secret = "bibby";
-int numOfLives = 3;
+﻿//string secret = "bibby";
+//int numOfLives = 3;
 
-Console.WriteLine("You have 3 lives.");
-Console.Write("Enter the secret word: ");
-string choice = Console.ReadLine();
-numOfLives--;
+//Console.WriteLine("You have 3 lives.");
+//Console.Write("Enter the secret word: ");
+//string choice = Console.ReadLine();
+//numOfLives--;
 
-while (choice != secret && numOfLives > 0)
-{
-    Console.WriteLine("You have {0} lives.", numOfLives);
-    Console.Write("Enter the secret word: ");
-    choice = Console.ReadLine();
-    numOfLives--;
-}
+//while (choice != secret && numOfLives > 0)
+//{
+//    Console.WriteLine("You have {0} lives.", numOfLives);
+//    Console.Write("Enter the secret word: ");
+//    choice = Console.ReadLine();
+//    numOfLives--;
+//}
 
-if  (choice == secret)
-{
-    Console.WriteLine(" You win");
-}
-else
-{
-    Console.WriteLine(" You Lose");
+//if  (choice == secret)
+//{
+//    Console.WriteLine(" You win");
+//}
+//else
+//{
+//    Console.WriteLine(" You Lose");
 
-}
+//}
+//second code
+
+//Console.WriteLine(GetPow(3,5));
+
+//static int GetPow(int baseNum, int powNum)
+
+//{
+//    int pow = baseNum;
+
+//    for (int i = 1; i < powNum; i++)
+//    {
+//        pow = pow*baseNum;
+//    }
+
+//    return pow;
+//}
+// third code with objects and classes and methods
+
+using HelloCsharp;
+
+Book book1 = new Book("diary of a wimpy kid", "jeff kiney", 400);
+Book book2 = new Book("BFG", "roald dahl", 100);
+
+Console.WriteLine(book2.IsNovel());
+Console.WriteLine(book1.FirstName());
+
